d6c7206d0f305ea3786b1eefcb68172e174b37f8
danilo.pereiraferreira <danilo.pereiraferreira@redacreltd.com>
2024-12-04 00:31:21 +0100

    style: Add husky and commit lint

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-empty": [2, "never"],
+    "subject-case": [2, "always", "sentence-case"],
+    "subject-max-length": [2, "always", 60],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "style",
+        "test",
+      ],
+    ],
+  },
+};
